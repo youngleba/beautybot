0a099a20b370b8b0fa326cf8745b469c3ae504db
youngleba <youngleba7@gmail.com>
2025-10-08 21:48:08 +0200

    Delete app/handlers.py

--- app/handlers.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types
-from aiogram.dispatcher import Dispatcher
-
-def register_handlers(dp: Dispatcher):
-    @dp.message_handler(commands=['start'])
-    async def start(msg: types.Message):
-        await msg.answer("Привет! 👋 Это BeautyBot. Запишись на услугу!")
