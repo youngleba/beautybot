bc013577b937f89fb0488d779aafc181a2e749bf
youngleba <youngleba7@gmail.com>
2025-10-08 22:00:16 +0200

    Create db.py

---
+++ app/database/db.py
@@ -0,0 +1,22 @@
+import asyncpg
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+async def create_db():
+    conn = await asyncpg.connect(DATABASE_URL)
+
+    # читаем SQL из файла schema.sql
+    schema_path = os.path.join(os.path.dirname(__file__), "schema.sql")
+    if os.path.exists(schema_path):
+        with open(schema_path, "r", encoding="utf-8") as f:
+            sql_script = f.read()
+        await conn.execute(sql_script)
+        print("✅ Таблицы из schema.sql успешно созданы или уже существуют.")
+    else:
+        print("⚠️ Файл schema.sql не найден!")
+
+    await conn.close()
