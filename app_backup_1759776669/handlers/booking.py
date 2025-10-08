3ede03ec1d83690960682338040ae0ea4872f080
youngleba <youngleba7@gmail.com>
2025-10-08 18:03:45 +0200

    Delete app_backup_1759776669 directory

--- app_backup_1759776669/handlers/booking.py
+++
@@ -1,8 +0,0 @@
-# ~/beautybot/app/handlers/booking.py
-from aiogram import Router
-
-router = Router()
-
-# Тут можно будет вынести более сложный flow бронирования (выбор даты/времени и т.д.)
-# Сейчас логика простая и находится в client.py
-
