f48002820446fb9ef39467c56de8575235d8f065
youngleba <youngleba7@gmail.com>
2025-10-09 14:13:02 +0200

    Update config_loader.py

--- app/utils/config_loader.py
+++ app/utils/config_loader.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MASTER_ID = int(os.getenv("MASTER_ID"))
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv
