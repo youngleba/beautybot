aa848dc567924f58d0dd641e36e478d4fc5662e1
youngleba <youngleba7@gmail.com>
2025-10-09 19:46:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.handlers import register_handlers
 from app.database.db import create_db
 from app.utils.config_loader import BOT_TOKEN
 
+# Настройка логирования
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)s %(message)s',
@@ -13,6 +14,7 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
@@ -25,10 +27,13 @@ async def main():
     register_handlers(dp)
 
     logging.info("✅ BeautyBot запущен и готов к работе.")
+
     try:
         await dp.start_polling(bot)
     except Exception as e:
         logging.error(f"Ошибка poll: {e}")
+    finally:
+        await bot.session.close()  # Корректное закрытие сессии бота
 
 if __name__ == "__main__":
     asyncio.run(main())
