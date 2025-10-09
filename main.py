dc48c0cd39c519b959d43abc11eac33c5c7cabfc
youngleba <youngleba7@gmail.com>
2025-10-09 15:55:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from app.handlers import register_handlers  # Регистрируем все роутеры
+from app.handlers import register_handlers
 from app.database.db import create_db
 from app.utils.config_loader import BOT_TOKEN
-
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    # Создаем базу данных (таблицы) при старте
     await create_db()
-
-    # Регистрируем хендлеры
     register_handlers(dp)
 
     print("✅ BeautyBot запущен и готов к работе.")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
