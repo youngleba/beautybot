2204df7de0cf0f6087e5c514b4f22cf1a0cda271
youngleba <youngleba7@gmail.com>
2025-10-09 20:50:20 +0200

    Update config_loader.py

--- app/utils/config_loader.py
+++ app/utils/config_loader.py
@@ -1,8 +1,24 @@
 import os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MASTER_ID = int(os.getenv("MASTER_ID"))
+MASTER_ID = os.getenv("MASTER_ID")
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if BOT_TOKEN is None:
+    logging.error("Ошибка: переменная окружения BOT_TOKEN не установлена!")
+
+if MASTER_ID is None:
+    logging.error("Ошибка: переменная окружения MASTER_ID не установлена!")
+else:
+    try:
+        MASTER_ID = int(MASTER_ID)
+    except ValueError:
+        logging.error("Ошибка: переменная MASTER_ID должна быть числом!")
+        MASTER_ID = None
+
+if DATABASE_URL is None:
+    logging.error("Ошибка: переменная окружения DATABASE_URL не установлена!")
