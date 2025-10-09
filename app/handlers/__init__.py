fc89f6b185c8f30f935ed850b29a720de770d449
youngleba <youngleba7@gmail.com>
2025-10-09 15:59:13 +0200

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,7 +2,6 @@ from aiogram import Dispatcher
 from app.handlers.client import booking
 from app.handlers.master import master_panel
 
-
 def register_handlers(dp: Dispatcher):
     dp.include_router(booking.router)
     dp.include_router(master_panel.router)
