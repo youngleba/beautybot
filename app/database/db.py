af302347057948f6e0df02738e93ac66807674d1
youngleba <youngleba7@gmail.com>
2025-10-09 19:51:13 +0200

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -1,18 +1,24 @@
 import asyncpg
 import os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def create_db():
-    conn = await asyncpg.connect(DATABASE_URL)
-    schema_path = os.path.join(os.path.dirname(__file__), "schema.sql")
-    if os.path.exists(schema_path):
-        with open(schema_path, 'r', encoding='utf-8') as f:
-            sql_script = f.read()
-        await conn.execute(sql_script)
-        print("✅ Таблицы из schema.sql успешно созданы или уже существуют.")
-    else:
-        print("⚠️ Файл schema.sql не найден!")
-    await conn.close()
+    try:
+        conn = await asyncpg.connect(DATABASE_URL)
+        schema_path = os.path.join(os.path.dirname(__file__), "schema.sql")
+        if os.path.exists(schema_path):
+            with open(schema_path, 'r', encoding='utf-8') as f:
+                sql_script = f.read()
+            await conn.execute(sql_script)
+            logging.info("✅ Таблицы из schema.sql успешно созданы или уже существуют.")
+        else:
+            logging.warning("⚠️ Файл schema.sql не найден!")
+        await conn.close()
+    except Exception as e:
+        logging.error(f"Ошибка при создании базы данных: {e}")
+        raise
