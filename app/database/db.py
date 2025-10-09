ac757f290f6b1e96b557e794f5b8f239d3b1de31
youngleba <youngleba7@gmail.com>
2025-10-09 15:56:14 +0200

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -3,22 +3,16 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def create_db():
-    """
-    Подключаемся к базе и создаём таблицы из schema.sql
-    """
     conn = await asyncpg.connect(DATABASE_URL)
     schema_path = os.path.join(os.path.dirname(__file__), "schema.sql")
-
     if os.path.exists(schema_path):
-        with open(schema_path, "r", encoding="utf-8") as f:
+        with open(schema_path, 'r', encoding='utf-8') as f:
             sql_script = f.read()
         await conn.execute(sql_script)
         print("✅ Таблицы из schema.sql успешно созданы или уже существуют.")
     else:
         print("⚠️ Файл schema.sql не найден!")
-
     await conn.close()
