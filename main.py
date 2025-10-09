22fc362d47c8adf4adb1cff8bd7e71b7df1bea29
youngleba <youngleba7@gmail.com>
2025-10-09 17:04:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from app.handlers import register_handlers
 from app.database.db import create_db
 from app.utils.config_loader import BOT_TOKEN
 
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='bot.log',
+    filemode='a',
+)
+
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    await create_db()
+    try:
+        await create_db()
+    except Exception as e:
+        logging.error(f"Ошибка при создании базы данных: {e}")
+
     register_handlers(dp)
 
-    print("✅ BeautyBot запущен и готов к работе.")
-    await dp.start_polling(bot)
+    logging.info("✅ BeautyBot запущен и готов к работе.")
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        logging.error(f"Ошибка poll: {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
